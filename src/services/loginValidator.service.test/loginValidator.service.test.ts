2043fd74aab67a3a6afba5cc01ce6862fbac9611
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2019-06-22 20:59:27 +0200

    Work in progress on unit test. Validate password.

--- src/services/loginValidator.service.test/loginValidator.service.test.ts
+++ src/services/loginValidator.service.test/loginValidator.service.test.ts
@@ -1,4 +1,5 @@
 import LoginValidatorService from "../../services/loginValidator.service";
+import locale from "../../locale/en";
 
 describe("services/queue.service.ts", () => {
   let loginValidator: LoginValidatorService = new LoginValidatorService();
@@ -8,12 +9,40 @@ describe("services/queue.service.ts", () => {
   });
 
   it("Validates username correctly", () => {
-    const correctUsername = "User";
-    const usernameValid = loginValidator.checkUsername(correctUsername);
+    const validUsername = "User";
+    const usernameValid = loginValidator.checkUsername(validUsername);
     expect(usernameValid.isValid).toBe(true);
 
-    const incorrectUsername = "";
-    const usernameInvalid = loginValidator.checkUsername(incorrectUsername);
+    const invalidUsername = "";
+    const usernameInvalid = loginValidator.checkUsername(invalidUsername);
     expect(usernameInvalid.isValid).toBe(false);
+  });
+
+  it("Validates valid password correctly", () => {
+    const validPassword = "kkabcdd";
+    const usernameValid = loginValidator.checkUsername(validPassword);
+    expect(usernameValid.isValid).toBe(true);
+  });
+
+  it("Validates password correctly - isEmptyPassword", () => {
+    const validPassword = "a";
+    let validationEmpty = loginValidator.checkIfStringIsEmpty(validPassword);
+    expect(validationEmpty.isValid).toBe(true);
+
+    const invalidPassword = "";
+    validationEmpty = loginValidator.checkIfStringIsEmpty(invalidPassword);
+    expect(validationEmpty.isValid).toBe(false);
+
+    const passwordIsValid = loginValidator.checkPassword(invalidPassword);
+    expect(passwordIsValid.isValid).toBe(false);
+    expect(passwordIsValid.message).toEqual(locale.validationErrors.isEmptyPassword);
+  });
+
+  it("Validates incorrect - containsAlphabetSequence", () => {
+    const incorrectPassword = "a";
+    const passwordIsValid = loginValidator.checkPassword(incorrectPassword);
+
+    expect(passwordIsValid.isValid).toBe(false);
+    expect(passwordIsValid.message).toEqual(locale.validationErrors.containsAlphabetSequence);
   });
 });
