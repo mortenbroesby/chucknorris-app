ee9e6306f842032455bf1fdf5d6788db2d709ad7
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2019-06-23 14:36:20 +0200

    Remove fade on login toast-message.

--- src/components/toastMessage/toastMessage.vue
+++ src/components/toastMessage/toastMessage.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="toastMessage">
     <div class="toastMessage--inner">
-      <transition name="fadeToast">
-        <div class="toastItem" v-show="isVisible">
-          <p class="toastItem--title">Note:</p>
-          <p class="toastItem--message">{{ message }}</p>
-        </div>
-      </transition>
+      <div class="toastItem" v-show="isVisible">
+        <p class="toastItem--title">Note:</p>
+        <p class="toastItem--message">{{ message }}</p>
+      </div>
     </div>
   </div>
 </template>
