fa8883caed56fc35d017ad77ae3c4a346499ba22
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2019-06-23 02:23:26 +0200

    Add styling to not-found page.

--- src/layouts/notFound/index.ts
+++ src/layouts/notFound/index.ts
@@ -3,31 +3,13 @@ import { Component } from "vue-property-decorator";
 import { RouteName } from "../../router";
 
 import template from "./notFound.vue";
-import { $store, RootState } from "../../store";
+import "./notFound.scss";
 
 @Component({
   mixins: [template],
   components: {}
 })
 export default class NotFound extends Vue {
-  /*************************************************/
-  /* COMPUTED'S */
-  /*************************************************/
-  get store(): RootState {
-    return $store.state;
-  }
-
-  get userIsAuthenticated(): boolean {
-    return this.store.userIsAuthenticated;
-  }
-
-  /*************************************************/
-  /* LIFE CYCLE */
-  /*************************************************/
-  mounted() {
-    this.redirectUser();
-  }
-
   /*************************************************/
   /* METHODS */
   /*************************************************/
--- src/layouts/notFound/notFound.vue
+++ src/layouts/notFound/notFound.vue
@@ -1,5 +1,20 @@
 <template>
-  <div id="notFound">
-    Not found
+  <div id="notFound" class="notFound">
+    <div class="chuckNorris"></div>
+    <div class="centerContent">
+      <div class="contentContainer">
+        <div class="header">
+          <h1 class="title">- Page not found -</h1>
+          <h1 class="title title--bold">Chuck Norris disapproves!</h1>
+        </div>
+        <div class="body">
+          <button
+            class="button"
+            @click="redirectUser()">
+            Go to Home
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
